af5edf507e9f7657bc7accf313b513fa0ebbea87
Cuong Dang <cuongdh.work@gmail.com>
2018-08-01 22:51:40 +0800

    Add tax calculation as a module

---
+++ app/models/concerns/taxable.rb
@@ -0,0 +1,20 @@
+module Concerns
+  module Taxable
+    extend ActiveSupport::Concern
+
+    BASIC_PERCENTAGE = 0.1
+    IMPORT_PERCENTAGE = 0.05
+
+    def sales_tax_rate
+      basic_tax + import_tax
+    end
+
+    def basic_tax_rate
+      try(:tax_exempted) ? 0 : BASIC_PERCENTAGE
+    end
+
+    def import_tax_rate
+      try(:type) != 'ImportedProduct' ? 0 : IMPORT_PERCENTAGE
+    end
+  end
+end
